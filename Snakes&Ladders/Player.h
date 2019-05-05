b81e257819d48e55d798119794e7e7c168e2b9ca
LuizRamos <luizramospe@hotmail.com>
2019-05-05 02:36:29 -0400

    Set Up the Game Logic

--- Snakes&Ladders/Player.h
+++ Snakes&Ladders/Player.h
@@ -13,6 +13,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface Player : NSObject
 //Properties
 @property (nonatomic) NSInteger currentSquare;
+@property (nonatomic, assign) NSDictionary * gameLogic;
 //Methods
 -(void) roll;
 @end
